f5ca134e1e2d28b5c8d963ef15e79446c8cc200a
Riya Batla <91269087+cereal-hecker@users.noreply.github.com>
2022-10-28 22:25:53 +0530

    Add files via upload

---
+++ frontend/src/components/Output.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import NonCarousel from "./NonCarousel";
+
+export default function Output() {
+
+    return (
+      <div id="output-page" className="container-fluid p-0">
+      <NonCarousel />
+      </div>
+    );
+  }
+  
