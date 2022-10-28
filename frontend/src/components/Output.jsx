e518c9069cc30c47eb5326bd4e9e1ca03d8d3f02
Riya Batla <91269087+cereal-hecker@users.noreply.github.com>
2022-10-29 00:09:29 +0530

    Update Output.jsx

--- frontend/src/components/Output.jsx
+++ frontend/src/components/Output.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import NonCarousel from "./NonCarousel";
+import Range from "./Range";
 
 export default function Output() {
 
     return (
       <div id="output-page" className="container-fluid p-0">
+      <Range />
       <NonCarousel />
       </div>
     );
